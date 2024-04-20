4301fe521d1f4b17a45c9730df99194f62680fab
techpilot <ngwustephen99@gmail.com>
2024-04-20 12:04:01 -0700

    implemented the contact section ui

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,11 +6,11 @@ import time_icon from "@/assets/svgs/time-icon.svg";
 
 const Contact = () => {
   return (
-    <div className="px-3 md:px-20 py-[2rem] flex flex-col gap-[2rem]">
+    <div className="px-3 md:px-20 md:py-[2rem] flex flex-col gap-[2rem]">
       <p className="text-center text-xl lg:text-3xl font-semibold text-[#012970] capitalize">
         Contact Us
       </p>
-      <div className="h-[26rem] flex flex-col md:flow-row items-center justify-between gap-5">
+      <div className="h-[26rem] flex flex-col lg:flex-row items-center justify-between gap-5">
         <div className="w-full h-full flex flex-col gap-5">
           <div className="h-[50%] flex justify-between gap-5">
             <div className="h-full w-[50%] bg-[#fafbff] rounded-md p-7 flex flex-col gap-2 justify-around">
